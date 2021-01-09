2b9e8baa43f277ad34be66a53f4ca3be1c919089
Carlos Zansavio <undefined751@gmail.com>
2021-01-09 11:50:37 -0300

    user-service e usertoken

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { map } from 'rxjs/operators';
+import { UserToken } from '../models/user-token';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private tokenSubject: BehaviorSubject<UserToken>;
+  public token: Observable<UserToken>;
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) {
+    this.tokenSubject = new BehaviorSubject<UserToken>(JSON.parse(localStorage.getItem('user-token')));
+    this.token = this.tokenSubject.asObservable();
+   }
+
+  public get tokenValue(): UserToken {
+    return this.tokenSubject.value;
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<UserToken>(`localhost:3000/users/login`, {email, password})
+    .pipe(
+      map(token => {
+        const userToken: UserToken = token;
+
+        localStorage.setItem('user-token', JSON.stringify(userToken));
+        this.tokenSubject.next(userToken);
+
+        return userToken;
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('user-token');
+    this.tokenSubject.next(null);
+    this.router.navigate(['/account/login']);
+  }
+}
